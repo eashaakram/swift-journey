f28544af66e6db120fdf4e45314ae1c46cfa1138
eashaakram <eashaakram63@gmail.com>
2026-07-07 02:42:56 +0500

    optional or unwrapp

--- Learning/Basics/optional_or_unwrapp.swift
+++ Learning/Basics/optional_or_unwrapp.swift
@@ -16,3 +16,17 @@ var mainstr : String?  //nil
 mainstr = "Yupp!"
 print("Without force unwrapping: \(mainstr)")  //it will print Optional("Yupp!")
 print("With force unwrapping: \(mainstr!)")  //it will print Yupp!
+
+if mainstr != nil {
+    print(mainstr!)  // ! unwrapp
+} else {
+    print("mainstr is nil")
+}
+
+
+// lets take another example
+var name : String = "Eesha" //without ? means it must contain value
+var nickname : String? = nil
+print(name, nickname ?? "none") //here ?? means if nickname contain value print leftside otherwise print rightside
+nickname = "Sha's"
+print(name, nickname ?? "none")
