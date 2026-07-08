d4421ef3e76e7f851f7029ca13f54956eecd76d8
eashaakram <eashaakram63@gmail.com>
2026-07-08 19:44:13 +0500

    array

--- Learning/Basics/array.swift
+++ Learning/Basics/array.swift
@@ -3,6 +3,13 @@ var myarr = [String]() // alloc init
 myarr = ["yy", "zz", "aa"]
 print(myarr)
 
+
+// Array Initialization
+let stars : [String] = ["Sun", "Moon", "Earth"]
+print(stars)
+print("Second value: \(stars[1])") // Moon
+
+
 // Working String
 let age = 20
 let name = "Easha"
@@ -11,8 +18,8 @@ let s1 = name + " is " + String(age) // convert Int to String
 print("First method: \(s1)")
 let s2 = "\(name) is \(age)" 
 print("Second method: \(s2)")
-
-// Array Initialization
-let stars : [String] = ["Sun", "Moon", "Earth"]
-print(stars)
-print("Second value: \(stars[1])") // Moon
+var n1 = 10
+var n2 = 20
+var n3 = 30
+var show = "First number " + String(n1) + " Second number " + String(n2) + " Third number " + String(n3)
+print("Third method: \(show)")
