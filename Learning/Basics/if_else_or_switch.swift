448c669c4784380e54b32aa37d9005aa08e87fe1
eashaakram <eashaakram63@gmail.com>
2026-07-09 20:02:42 +0500

    if-else or switch

---
+++ Learning/Basics/if_else_or_switch.swift
@@ -0,0 +1,19 @@
+// Control Flow
+// if-else
+if 10 > 5 {
+    print("10 is greater than 5")
+} else {
+    print("10 is not greater than 5")
+}
+
+// switch
+let name1 = "Easha"
+switch name1 {
+    case "Akram":
+        print("Name is Akram")
+    case "Easha":
+        print("Name is Easha")
+    default:
+        print("Irrelevant name")
+}
+
