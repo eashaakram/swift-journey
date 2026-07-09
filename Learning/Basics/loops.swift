ab6d9aaf2aa4c99cd3b02d8f7abafefe06978071
eashaakram <eashaakram63@gmail.com>
2026-07-09 20:21:08 +0500

    loops

--- Learning/Basics/loops.swift
+++ Learning/Basics/loops.swift
@@ -5,8 +5,15 @@ for name in names {
     print("\(name)") // for in loop
 }
 
-for i in 1...10 { // here ... means upto 10
+for i in 1...9 { // here ... means upto 9
     if i % 3 == 0 {
         print(i, terminator: " ") // terminator: " " means print in same line with space
     }
+}
+print() // print new line
+
+// from, to, by
+let three = stride(from: 2, to: 10, by: 2) // here from means start from 2, to means upto 10, by means increment by 2
+for n in three {
+    print(n, terminator: " ")
 }
