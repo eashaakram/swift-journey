0c6ac483f080a453dd0f6ecea00a7d6cbecaa10f
eashaakram <eashaakram63@gmail.com>
2026-07-10 23:39:07 +0500

    calculator

---
+++ Practice/calculator_begin.swift
@@ -0,0 +1,39 @@
+var a : Int?
+while a == nil{
+print("Enter first value: ")
+if let input = readLine(){
+    if let num = Int(input){
+        a = num
+}else{
+    print("Invalid number\nTry again")
+}
+}
+}
+var b : Int?
+while b == nil{
+print("Enter second value: ")
+if let input = readLine(){
+    if let num = Int(input){
+        b = num
+}else{
+    print("Invalid number\nTry again")
+
+}
+}
+} 
+print(=== CALCULATOR MENU ===)
+print("Enter + for addition and - for subtraction)
+print("Enter your choice: ", terminator: " ")
+if let operator = readLine() 
+while(true){
+switch operator {
+case "+": 
+print("Sum of \(a) and \(b) is \(a+b)")
+
+case "-": 
+print("Subtraction of \(a) and \(b) is \(a-b)")
+default:
+print("Invalid choice\nEnter again: ", terminator: " ")
+}
+} 
+
