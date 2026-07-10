11196a1d6ab7472468a2f0027c5ee59ef7859d9b
eashaakram <eashaakram63@gmail.com>
2026-07-10 23:59:43 +0500

    calculator

--- Practice/calculator_begin.swift
+++ Practice/calculator_begin.swift
@@ -1,38 +1,41 @@
-var a : Int?
-while a == nil{
-print("Enter first value: ")
-if let input = readLine(){
-    if let num = Int(input){
-        a = num
-}else{
-    print("Invalid number\nTry again")
+var a: Int?
+while a == nil {
+    print("Enter first value: ", terminator: "")
+    if let input = readLine() {
+        if let num = Int(input) {
+            a = num
+        } else {
+            print("Invalid number\nTry again")
+        }
+    }
 }
+var b: Int?
+while b == nil {
+    print("Enter second value: ", terminator: "")
+    if let input = readLine() {
+        if let num = Int(input) {
+            b = num
+        } else {
+            print("Invalid number\nTry again")
+        }
+    }
 }
-}
-var b : Int?
-while b == nil{
-print("Enter second value: ")
-if let input = readLine(){
-    if let num = Int(input){
-        b = num
-}else{
-    print("Invalid number\nTry again")
-
-}
-}
-} 
 print("=== CALCULATOR MENU ===")
 print("Enter + for addition and - for subtraction")
-print("Enter your choice: ", terminator: " ")
-if let op = readLine() {
-switch op {
-case "+": 
-print("Sum of \(a) and \(b) is \(a+b)")
-
-case "-": 
-print("Subtraction of \(a) and \(b) is \(a-b)")
-default:
-print("Invalid choice\nEnter again: ", terminator: " ")
+while true {
+    print("Enter your choice: ", terminator: "")
+    if let op = readLine() {
+        switch op {
+        case "+":
+            print("Sum of \(a!) and \(b!) is \(a! + b!)")
+            break
+        case "-":
+            print("Subtraction of \(a!) and \(b!) is \(a! - b!)")
+            break
+        default:
+            print("Invalid choice. Try again.")
+            continue
+        }
+        break
+    }
 }
-} 
-
