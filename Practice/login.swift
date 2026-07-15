36f481589e72afca980d10e138e1ccd105c2f3ae
eashaakram <eashaakram63@gmail.com>
2026-07-15 21:41:23 +0500

    login

--- Practice/login.swift
+++ Practice/login.swift
@@ -1,3 +1,5 @@
+// Login System
+// Get username
 func getUsername() -> String {
     while true {
         print("Enter username: ", terminator: "")
@@ -7,6 +9,18 @@ func getUsername() -> String {
             } else {
               return username  
             }
+        }
+    }
+}
+
+// Get password
+func getPassword() -> Int { 
+    while true {
+        print("Enter password in numbers: ", terminator: "")
+        if let input = readLine(), let password = Int(input) {
+            return password 
+        } else {
+            print("Invalid password\nPassword must contain numbers only")
         }
     }
 }
