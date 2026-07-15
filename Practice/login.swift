5897704bd782370722085838fd0f58c27eac5e04
eashaakram <eashaakram63@gmail.com>
2026-07-15 22:25:46 +0500

    login

--- Practice/login.swift
+++ Practice/login.swift
@@ -1,11 +1,58 @@
-// Login System
+// // Login System
+// // Get username
+// func getUsername() -> String {
+//     while true {
+//         print("Enter username: ", terminator: "")
+//         if let username = readLine() {
+//             if username.isEmpty {
+//                 print("Username is emply\nPlease enter any username")
+//             } else {
+//               return username  
+//             }
+//         }
+//     }
+// }
+
+// // Get password
+// func getPassword() -> Int { 
+//     while true {
+//         print("Enter password in numbers: ", terminator: "")
+//         if let input = readLine(), let password = Int(input) {
+//             return password 
+//         } else {
+//             print("Invalid password\nPassword must contain numbers only")
+//         }
+//     }
+// }
+
+// // login part
+// func login() {
+//     let correctUsername = "easha"
+//     let correctPassword = 12345678
+//     while true{
+//         let username = getUsername()
+//         let password = getPassword()
+//         if username == correctUsername && password == correctPassword {
+//             print("Login Successful!")
+//             return
+//         } else { 
+//             print("Invalid username and password \nEnter again!")
+//         }
+//     }
+// }
+
+
+// Program 
+login()
+
+// Level 2
 // Get username
 func getUsername() -> String {
     while true {
         print("Enter username: ", terminator: "")
         if let username = readLine() {
             if username.isEmpty {
-                print("Username is emply\nPlease enter any username")
+                print("Username is emply. Please enter any username...")
             } else {
               return username  
             }
@@ -14,13 +61,17 @@ func getUsername() -> String {
 }
 
 // Get password
-func getPassword() -> Int { 
+func getPassword() -> String { 
     while true {
         print("Enter password in numbers: ", terminator: "")
-        if let input = readLine(), let password = Int(input) {
-            return password 
-        } else {
-            print("Invalid password\nPassword must contain numbers only")
+        if let password = readLine() {
+            if password.isEmpty {
+                print("Password cannot be empty. Please enter your password...")
+            } else if password.count < 8 {
+                print("Password must be at least 8 characters long...")
+            } else { 
+                return password
+            }
         }
     }
 }
@@ -28,7 +79,7 @@ func getPassword() -> Int {
 // login part
 func login() {
     let correctUsername = "easha"
-    let correctPassword = 12345678
+    let correctPassword = "12345678"
     while true{
         let username = getUsername()
         let password = getPassword()
@@ -36,11 +87,7 @@ func login() {
             print("Login Successful!")
             return
         } else { 
-            print("Invalid username and password \nEnter again!")
+            print("Invalid username and password. Enter again!...")
         }
     }
 }
-
-
-// Program 
-login()
