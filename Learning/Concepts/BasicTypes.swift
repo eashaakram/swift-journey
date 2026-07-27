85955e46a4dd81ef4d9ba1a4797cecfc51927bab
eashaakram <eashaakram63@gmail.com>
2026-07-27 23:06:10 +0500

    comment

--- Learning/Concepts/BasicTypes.swift
+++ Learning/Concepts/BasicTypes.swift
@@ -1,3 +1,18 @@
 import Foundation
 var greeting = "Hello, playground"
 print(greeting)
+print(greeting)
+print(greeting)
+
+// This is a sigle-line comment
+// These are multiplr single-line comment
+// This is a comment 
+// and this is some more info
+// easha akram
+// Im learning swift
+
+/*
+ This is multi-line comment
+ that goes to 
+ multiple liners!
+*/
