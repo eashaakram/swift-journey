6dd3eb197a56571c21931f4620833d8dc96cafc8
eashaakram <eashaakram63@gmail.com>
2026-07-27 23:15:48 +0500

    Naming Conventions other methods

--- Learning/Concepts/BasicTypes.swift
+++ Learning/Concepts/BasicTypes.swift
@@ -27,4 +27,14 @@ let thisIsMyFirstGreeting = "Hello, World!" // This is Camel Case
 
 let thisismysecondgreeting = "Hello, World!" // This is also not wrong variable but this is hard to raed
 
+// Many developers prefer many others ways 
+// These are given below
 
+// Snake Case 
+let this_is_what_snake_case_looks_like = "Hello, World!"
+
+// Pascal Case
+let ThisIsWhatSnakeCaseLooksLike = "Hello, World!" 
+
+// Camel Case
+let thisIsWhatSnakeCaseLooksLike = "Hello, World!" // (HighlyRecommended)
