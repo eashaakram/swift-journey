20a4ad2167801dbf634fdeac143b91f0915d240c
eashaakram <eashaakram63@gmail.com>
2026-07-27 23:35:53 +0500

    basics

---
+++ Learning/Concepts/BasicTypes.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+// String is "regular text"
+let myFirstItem: String = "Hello, world!"
+
+// Refrence previously created objects
+let myTitle = myFirstItem
+
+// Boolean (aka Bool) is true or false
+let mySecondItem: Bool = true
+let myThirdItem: Bool = false
+
+// Do not do!
+// let myFourthItem: String = "true"  This is not Bool
+
+// Swift is type-safe language
+let myFifthItem: Bool = true
+let mySixthItem: String = "Hello, world!"
+let mySeventhItem = true
+
