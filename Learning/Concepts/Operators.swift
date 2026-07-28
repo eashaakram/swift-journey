fd08915b8dd0ef4875acd9f05e72d4f233f40504
eashaakram <eashaakram63@gmail.com>
2026-07-29 00:57:26 +0500

    if and if-else

--- Learning/Concepts/Operators.swift
+++ Learning/Concepts/Operators.swift
@@ -4,31 +4,58 @@ var likeCount: Double = 5
 var commentCount: Double = 0
 var viewCount: Double = 100
 
-// Addition
-// likeCount = 5 + 1 // Bad approach
-// likeCount = likeCount + 1 // Better approach
-likeCount += 1 // Best approach
-print("Plus: \(likeCount)")
+// // Addition
+// // likeCount = 5 + 1 // Bad approach
+// // likeCount = likeCount + 1 // Better approach
+// likeCount += 1 // Best approach
+// print("Plus: \(likeCount)")
 
-// Subtraction
-// likeCount = likeCount - 1
-likeCount -= 1
-print("Minus: \(likeCount)")
+// // Subtraction
+// // likeCount = likeCount - 1
+// likeCount -= 1
+// print("Minus: \(likeCount)")
 
-// Multiplication
-// likeCount = likeCount * 1.5
-likeCount *= 2
-print("Multiply: \(likeCount)")
+// // Multiplication
+// // likeCount = likeCount * 1.5
+// likeCount *= 2
+// print("Multiply: \(likeCount)")
 
-// Division
-// likeCount = likeCount / 2
-likeCount /= 2
-// print("Divide: \(likeCount)")
+// // Division
+// // likeCount = likeCount / 2
+// likeCount /= 2
+// // print("Divide: \(likeCount)")
 
-// Order of operations does matter!
-// PEMDAS
-// likeCount = likeCount - 1 * 2.0
-print(likeCount)
-// Use parenthesis
-likeCount = (likeCount - 1) * 2.0
-print("Parenthesis: \(likeCount)")
+// // Order of operations does matter!
+// // PEMDAS
+// // likeCount = likeCount - 1 * 2.0
+// print(likeCount)
+// // Use parenthesis
+// likeCount = (likeCount - 1) * 2.0
+// print("Parenthesis: \(likeCount)")
+
+// if and if-else
+if likeCount == 5 {
+    print("Post has 5 likes.")
+} else {
+    print("Post does NOT have 5 likes.")
+}
+
+if likeCount != 5 {
+    print("Post does NOT have 5 likes.")
+}
+
+if likeCount > 5 {
+    print("Post has greater than 5 likes.")
+}
+
+if likeCount >= 5 {
+    print("Post has greater than or equal to 5 likes.")
+}
+
+if likeCount < 5 {
+    print("Post has less than 5 likes.")
+}
+
+if likeCount <= 5 {
+    print("Post has less than or equal to 5 likes.")
+}
