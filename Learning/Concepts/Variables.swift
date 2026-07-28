7fa85e3ce1ecba5a59e5960e08a4b4555464282a
eashaakram <eashaakram63@gmail.com>
2026-07-28 18:53:38 +0500

    Variables

--- Learning/Concepts/Variables.swift
+++ Learning/Concepts/Variables.swift
@@ -1,81 +1,112 @@
-// Variables in Swift
+import Foundation
+// // Variables in Swift
 
-//let is used to declare a constant variable, which means its value cannot be changed once assigned.
-let constantValue = 10
-print("Immutable value: ", constantValue)
+// //let is used to declare a constant variable, which means its value cannot be changed once assigned.
+// let constantValue = 10
+// print("Immutable value: ", constantValue)
 
-//var is used to declare a variable, which means its value can be changed after it has been assigned.
-var variableValue = 20     
-print("Initial value: ", variableValue)
-variableValue = 30
-print("Updated value: ", variableValue)
-
-
-//lets take another example ffor better understanding
-let str = "Easha";
-//first here we came at a point the semicoln is optional in swift but if we want to write multiple statements in a single line then we can use semicoln to separate them.
-print("My name is \(str)"); print("I am learning Swift programming language");
-//str = "Akram"  This will give an error because str is a constant and cannot be changed.
-// - error: cannot assign to value: 'str' is a 'let' constant
-
-var name = "Eesha"
-print("Name: ", name)
-name = "Eesha Akram"
-print("Updated Name: ", name)
+// //var is used to declare a variable, which means its value can be changed after it has been assigned.
+// var variableValue = 20     
+// print("Initial value: ", variableValue)
+// variableValue = 30
+// print("Updated value: ", variableValue)
 
 
-//empty string
-let emptystr = ""
-//check it is empty or not
-if emptystr.isEmpty {
-    print("The string is empty")
-} else {
-    print("The string is not empty")
-}
+// //lets take another example ffor better understanding
+// let str = "Easha";
+// //first here we came at a point the semicoln is optional in swift but if we want to write multiple statements in a single line then we can use semicoln to separate them.
+// print("My name is \(str)"); print("I am learning Swift programming language");
+// //str = "Akram"  This will give an error because str is a constant and cannot be changed.
+// // - error: cannot assign to value: 'str' is a 'let' constant
+
+// var name = "Eesha"
+// print("Name: ", name)
+// name = "Eesha Akram"
+// print("Updated Name: ", name)
 
 
-//compare string
-var v1 = "Hello"
-var v2 = "Hello"
-if (v1 == v2) {
-    print("Both strings are Equal")
-} else {
-    print("Both strings are not Equal")
-}
-
-//print variable values
-var V1 = "Hello1"
-var V2 = "Hello"
-if (V1 == V2) {
-    print("\(V1) and \(V2) are Equal")
-} else {
-    print("\(V1) and \(V2) are not Equal")
-}
-
-let a = 2, b = 3
-print("Total: \(a + b)")
-
-var cv1 = "Godzilla"
-var cv2 = 1000.0
-print("Value of \(cv1) is more than \(cv2) millions")
+// //empty string
+// let emptystr = ""
+// //check it is empty or not
+// if emptystr.isEmpty {
+//     print("The string is empty")
+// } else {
+//     print("The string is not empty")
+// }
 
 
-//variable declare
-var va = 1212
-var vb : Float
-vb = 12.2
-var vc : Double = 12.2
-print("Value of va : \(va)"); print("Value of vb : \(vb)"); print("Value of vc : \(vc)")
+// //compare string
+// var v1 = "Hello"
+// var v2 = "Hello"
+// if (v1 == v2) {
+//     print("Both strings are Equal")
+// } else {
+//     print("Both strings are not Equal")
+// }
 
-let greeting = "Hello"
-var nam = "Swift"
-print(greeting + ", " + nam)
+// //print variable values
+// var V1 = "Hello1"
+// var V2 = "Hello"
+// if (V1 == V2) {
+//     print("\(V1) and \(V2) are Equal")
+// } else {
+//     print("\(V1) and \(V2) are not Equal")
+// }
 
-let pi: Double = 3.14
-var count: Int = 3
-print("pi: \(pi), count: \(count)")
+// let a = 2, b = 3
+// print("Total: \(a + b)")
 
-let a = 7 , b = 2
-print(a,b) // space separated by default 
-print("a = \(a), b = \(b)") //interpolation
-print(sum = \(a+b)) //inline math
+// var cv1 = "Godzilla"
+// var cv2 = 1000.0
+// print("Value of \(cv1) is more than \(cv2) millions")
+
+
+// //variable declare
+// var va = 1212
+// var vb : Float
+// vb = 12.2
+// var vc : Double = 12.2
+// print("Value of va : \(va)"); print("Value of vb : \(vb)"); print("Value of vc : \(vc)")
+
+// let greeting = "Hello"
+// var nam = "Swift"
+// print(greeting + ", " + nam)
+
+// let pi: Double = 3.14
+// var count: Int = 3
+// print("pi: \(pi), count: \(count)")
+
+// let a = 7 , b = 2
+// print(a,b) // space separated by default 
+// print("a = \(a), b = \(b)") //interpolation
+// print("sum = \(a+b)") //inline math
+
+
+// Practice
+
+
+// Constant 
+let someConstant = true
+
+// Variable
+var someVariable = true
+
+// Cannot assign to value: 'someConstant' is a 'let' constant
+// someConstant = false
+
+// use var when you think you need to change value
+someVariable = false
+
+
+var myNumber = 1.1234 // one time you choose its type double then when you change value its type will be same 
+print(myNumber)
+myNumber = 2
+print(myNumber)
+myNumber = 234567
+print(myNumber)
+myNumber = 1
+print(myNumber)
+myNumber = 458
+print(myNumber)
+
+
