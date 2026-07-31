537b49a05ff75d5a0c1e0abaf81bc14f7758c47d
eashaakram <eashaakram63@gmail.com>
2026-07-31 14:19:34 +0500

    Practice

--- Learning/Concepts/Functions.swift
+++ Learning/Concepts/Functions.swift
@@ -1,261 +1,261 @@
 import Foundation
-// Functions
+// // Functions
 
-// Simple function without parameters
-func backgroundChange() {
-    print("Background Color changed")
-}
-print("Calling function without parameters")
-backgroundChange()
+// // Simple function without parameters
+// func backgroundChange() {
+//     print("Background Color changed")
+// }
+// print("Calling function without parameters")
+// backgroundChange()
 
-// Multiple Arguments 
-func addvalues(value1: Int, value2: Int) -> Int { //here -> Int indicates the return type of the function
-    return value1 + value2
-}
-print("Calling function with parameters")
-let v1 = 10
-let v2 = 20
-let a = addvalues(value1: v1, value2: v2)
-print("Sum of \(v1) and \(v2) is \(a)")
+// // Multiple Arguments 
+// func addvalues(value1: Int, value2: Int) -> Int { //here -> Int indicates the return type of the function
+//     return value1 + value2
+// }
+// print("Calling function with parameters")
+// let v1 = 10
+// let v2 = 20
+// let a = addvalues(value1: v1, value2: v2)
+// print("Sum of \(v1) and \(v2) is \(a)")
 
-// Another example
-func familyName(name: String, age: Int) -> String {
-    return "My name is \(name) and I am \(age) years old"
-}
-let str = familyName(name: "Easha", age: 20)
-print(str)
+// // Another example
+// func familyName(name: String, age: Int) -> String {
+//     return "My name is \(name) and I am \(age) years old"
+// }
+// let str = familyName(name: "Easha", age: 20)
+// print(str)
 
-// Multiple return values
-func getLanguages() -> (String, String, String) {
-    return ("Swift", "C++", "Python")
-}
-// Program
-let languages = getLanguages()
-print("Access 3 language: \(languages.2)") // Python
-print("Language1: \(languages.0), Language2: \(languages.1), Languages3: \(languages.2)")
+// // Multiple return values
+// func getLanguages() -> (String, String, String) {
+//     return ("Swift", "C++", "Python")
+// }
+// // Program
+// let languages = getLanguages()
+// print("Access 3 language: \(languages.2)") // Python
+// print("Language1: \(languages.0), Language2: \(languages.1), Languages3: \(languages.2)")
 
-print("Another method")
-func getProgram() -> (language1: String, language2: String) {
-    return ("Swift", "C++")
-}
-//Program
-let program = getProgram()
-print("Language1: \(program.language1), Language2: \(program.language2)")
-
-
-
-// Below code will not handle error if user enter string instead of int
-print("Method 1")
-// Function with multiple arguments
-func add_values(Value1: Int, Value2: Int) -> Int {
-    return Value1 + Value2
-}
-// Program
-print("Enter first value:")
-let V1 = Int(readLine()!)!
-print("Enter  second value:")
-let V2 = Int(readLine()!)!
-let A = add_values(Value1: V1, Value2: V2)
-print("Sum of \(V1) and \(V2) is \(A)")
-
-// below code will not crash if user enter invalid input
-print("Method 2")
-
-func addValues(value1: Int, value2: Int) -> Int {
-    return value1 + value2
-}
-// Program
-print("Enter first value:")
-if let input1 = readLine(), let n1 = Int(input1) {
-    print("Enter second value:")
-    if let input2 = readLine(), let n2 = Int(input2) {
-        let sum = addValues(value1: n1, value2: n2)
-        print("Sum of \(n1) and \(n2) is \(sum)")
-    } else {
-        print("Invalid second number.")
-    }
-} else {
-    print("Invalid first number.")
-}
+// print("Another method")
+// func getProgram() -> (language1: String, language2: String) {
+//     return ("Swift", "C++")
+// }
+// //Program
+// let program = getProgram()
+// print("Language1: \(program.language1), Language2: \(program.language2)")
 
 
-// Below code will take input from user until it enter valid input
-print("Method 3")
 
-// Function to add two values
-func Addvalues(value1: Int, value2: Int) -> Int {
-    return value1 + value2
-}
-// Function to get a valid integer from the user
-func getValidInteger(message: String) -> Int {
-    while true {
-        print(message)
-        if let input = readLine(),
-           let number = Int(input) {
-            return number
-        } else {
-            print("Invalid input! \nPlease enter a valid integer.")
-        }
-    }
-}
-// Program starts here
-let N1 = getValidInteger(message: "Enter first value:")
-let N2 = getValidInteger(message: "Enter second value:")
-let sum = Addvalues(value1: N1, value2: N2)
-print("Sum of \(N1) and \(N2) is \(sum)")
+// // Below code will not handle error if user enter string instead of int
+// print("Method 1")
+// // Function with multiple arguments
+// func add_values(Value1: Int, Value2: Int) -> Int {
+//     return Value1 + Value2
+// }
+// // Program
+// print("Enter first value:")
+// let V1 = Int(readLine()!)!
+// print("Enter  second value:")
+// let V2 = Int(readLine()!)!
+// let A = add_values(Value1: V1, Value2: V2)
+// print("Sum of \(V1) and \(V2) is \(A)")
 
-// Practice
+// // below code will not crash if user enter invalid input
+// print("Method 2")
 
-func myFirstFunction() {
-    print("MY FIRST FUNCTION CALLED")
-    mySecondFunction()  // calling second function inside first
-    myThirdFunction()
-}
-
-func mySecondFunction() {
-    print("MY SECOND FUNCTION CALLED")
-}
-
-func myThirdFunction() {
-    print("MY THIRD FUNCTION CALLED")
-}
-
-myFirstFunction() // calling
+// func addValues(value1: Int, value2: Int) -> Int {
+//     return value1 + value2
+// }
+// // Program
+// print("Enter first value:")
+// if let input1 = readLine(), let n1 = Int(input1) {
+//     print("Enter second value:")
+//     if let input2 = readLine(), let n2 = Int(input2) {
+//         let sum = addValues(value1: n1, value2: n2)
+//         print("Sum of \(n1) and \(n2) is \(sum)")
+//     } else {
+//         print("Invalid second number.")
+//     }
+// } else {
+//     print("Invalid first number.")
+// }
 
 
-// more examples
-func getUserName() {
-    let username = "Easha"
-    print(username)
-}
+// // Below code will take input from user until it enter valid input
+// print("Method 3")
 
-// let name = username  we cannot use username outside because it is private
-getUserName()
+// // Function to add two values
+// func Addvalues(value1: Int, value2: Int) -> Int {
+//     return value1 + value2
+// }
+// // Function to get a valid integer from the user
+// func getValidInteger(message: String) -> Int {
+//     while true {
+//         print(message)
+//         if let input = readLine(),
+//            let number = Int(input) {
+//             return number
+//         } else {
+//             print("Invalid input! \nPlease enter a valid integer.")
+//         }
+//     }
+// }
+// // Program starts here
+// let N1 = getValidInteger(message: "Enter first value:")
+// let N2 = getValidInteger(message: "Enter second value:")
+// let sum = Addvalues(value1: N1, value2: N2)
+// print("Sum of \(N1) and \(N2) is \(sum)")
 
-// return
-func returnUserName() -> String {
-    let username: String = "Easha Akram"
-    return username
-}
+// // Practice
 
-let name: String = returnUserName()
+// func myFirstFunction() {
+//     print("MY FIRST FUNCTION CALLED")
+//     mySecondFunction()  // calling second function inside first
+//     myThirdFunction()
+// }
 
-// ------------------------------------------------------------------------------------
+// func mySecondFunction() {
+//     print("MY SECOND FUNCTION CALLED")
+// }
+
+// func myThirdFunction() {
+//     print("MY THIRD FUNCTION CALLED")
+// }
+
+// myFirstFunction() // calling
 
 
-// var userDidCompleteOnBoarding: Bool = false 
-// var userProfileIsCreated: Bool = true
+// // more examples
+// func getUserName() {
+//     let username = "Easha"
+//     print(username)
+// }
 
-// func checkUserStatus() -> Bool {
-//     if userDidCompleteOnBoarding && userProfileIsCreated {
-//         return true
+// // let name = username  we cannot use username outside because it is private
+// getUserName()
+
+// // return
+// func returnUserName() -> String {
+//     let username: String = "Easha Akram"
+//     return username
+// }
+
+// let name: String = returnUserName()
+
+// // ------------------------------------------------------------------------------------
+
+
+// // var userDidCompleteOnBoarding: Bool = false 
+// // var userProfileIsCreated: Bool = true
+
+// // func checkUserStatus() -> Bool {
+// //     if userDidCompleteOnBoarding && userProfileIsCreated {
+// //         return true
+// //     } else {
+// //         return false
+// //     }
+// // }
+
+// // parenthesis
+
+// showFirstScreen()
+// func showFirstScreen() {
+//     var userDidCompleteOnboarding = false
+//     var userProfileIscreated = true
+//     let status = checkUserstatus(didCompleteOnBoarding: userDidCompleteOnboarding, profileIsCreated: userProfileIscreated)
+
+//     if status == true {
+//         print("SHOW HOME SCREEN")
+//     } else {
+//         print("SHOW ONBOARDING SCREEN")
+//     }
+// }
+
+// func checkUserstatus (didCompleteOnBoarding: Bool, profileIsCreated: Bool) -> Bool {
+//     if didCompleteOnBoarding && profileIsCreated {
+//         return true 
 //     } else {
 //         return false
 //     }
 // }
 
-// parenthesis
 
-showFirstScreen()
-func showFirstScreen() {
-    var userDidCompleteOnboarding = false
-    var userProfileIscreated = true
-    let status = checkUserstatus(didCompleteOnBoarding: userDidCompleteOnboarding, profileIsCreated: userProfileIscreated)
+// // ------------------------------------------------------------------------------------
 
-    if status == true {
-        print("SHOW HOME SCREEN")
-    } else {
-        print("SHOW ONBOARDING SCREEN")
-    }
-}
+// doSomething()
+// doSomethingelse()
 
-func checkUserstatus (didCompleteOnBoarding: Bool, profileIsCreated: Bool) -> Bool {
-    if didCompleteOnBoarding && profileIsCreated {
-        return true 
-    } else {
-        return false
-    }
-}
+// func doSomething() -> Void {
+//    // print("EXECUTE-1")
+//    var isNew: Bool = false
 
+//    if isNew {
+//     print("NEW")
+//    } else {
+//     print("NOT NEW")
+//    }
+// }
 
-// ------------------------------------------------------------------------------------
-
-doSomething()
-doSomethingelse()
-
-func doSomething() -> Void {
-   // print("EXECUTE-1")
-   var isNew: Bool = false
-
-   if isNew {
-    print("NEW")
-   } else {
-    print("NOT NEW")
-   }
-}
-
-func doSomethingelse() -> String {
+// func doSomethingelse() -> String {
  
-   var isNew: Bool = false
+//    var isNew: Bool = false
 
-   if isNew {
-    return "NEW"
-   } 
-    return "FALSE"
-}
+//    if isNew {
+//     return "NEW"
+//    } 
+//     return "FALSE"
+// }
 
-func doMoreSomething() -> String {
-    var title: String = "Avengers"
+// func doMoreSomething() -> String {
+//     var title: String = "Avengers"
 
-    // *If title is equal to Avengers
-    if title == "Avengers" {
-        return "Marvel"
-    } else {
-    return "Not Marvel"
-    }
-}
+//     // *If title is equal to Avengers
+//     if title == "Avengers" {
+//         return "Marvel"
+//     } else {
+//     return "Not Marvel"
+//     }
+// }
 
-// ------------------------------------------------------------------------------------
+// // ------------------------------------------------------------------------------------
 
 
 
-// Guard
-doSomethingElse()
+// // Guard
+// doSomethingElse()
 
-func doSomethingElse() {
-    var title: String = "Avengers"
+// func doSomethingElse() {
+//     var title: String = "Avengers"
 
-    // Make sure title == Avengers
-    guard title == "Avengers" else {
-        print("Not Marvel")
-        return
-    }
-    print("Marvel")
-}
+//     // Make sure title == Avengers
+//     guard title == "Avengers" else {
+//         print("Not Marvel")
+//         return
+//     }
+//     print("Marvel")
+// }
 
-checkIfTitleIsAvengers()
+// checkIfTitleIsAvengers()
 
-func checkIfTitleIsAvengers() -> Bool {
-    var title: String = "Avengers"
+// func checkIfTitleIsAvengers() -> Bool {
+//     var title: String = "Avengers"
 
-    // Make sure title == Avengers
-    guard title == "Avengers" else {
-        return false
-    }
-    return true
-}
+//     // Make sure title == Avengers
+//     guard title == "Avengers" else {
+//         return false
+//     }
+//     return true
+// }
 
-checkIfTitleIsAvengers2()
+// checkIfTitleIsAvengers2()
 
-func checkIfTitleIsAvengers2() -> Bool {
-    var title: String = "Avengers"
+// func checkIfTitleIsAvengers2() -> Bool {
+//     var title: String = "Avengers"
 
-    // If title == Avengers
-    if title == "Avengers" {
-        return true
-    }
-    return false
-}
+//     // If title == Avengers
+//     if title == "Avengers" {
+//         return true
+//     }
+//     return false
+// }
 
 // ------------------------------------------------------------------------------------
 
@@ -274,12 +274,19 @@ func calculateNumbers() -> Int {
 //     return number1 + number2
 // }
 
-func calculateNumbers(valuer1: Int, value2: Int) -> Int {
+func calculateNumbers(value1: Int, value2: Int) -> Int {
     return number1 + number2
 }
 
 let result1 = calculateNumbers()
-let result2 = calculateNumbers(valuer1: number1, value2: number2)
+let result2 = calculateNumbers(value1: number1, value2: number2)
 
 print("Method 1: \(result1)")
 print("Method 2: \(result2)")
+
+func subtractNumbers(v1: Int, v2: Int) -> Int {
+    return v1 - v2
+}
+
+let result = subtractNumbers(v1: number1, v2: number2)
+print("Subtract: \(result)")
