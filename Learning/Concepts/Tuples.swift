d4bd006e1e69dd7f4ac0553cde38be05b4e05ead
eashaakram <eashaakram63@gmail.com>
2026-08-05 18:52:51 +0500

    Tuples are s calable

--- Learning/Concepts/Tuples.swift
+++ Learning/Concepts/Tuples.swift
@@ -73,3 +73,14 @@ func getUserInfo3() -> (name: String, isPremium: Bool) {
 
 let info2 = getUserInfo3()
 let name2 = info2.isPremium
+
+func getUserInfo4() -> (name: String, isPremium: Bool, isNew: Bool) {
+    return (userName, userIsPremium, userIsNew)
+}
+
+func doSomethingWithUserInfo(info: (name: String, isPremium: Bool, isNew: Bool)) {
+
+}
+
+let info = getUserInfo4()
+doSomethingWithUserInfo(info: info)
