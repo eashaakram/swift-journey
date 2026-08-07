474ab51d8f259783ee0beb095279f84f74bf1954
eashaakram <eashaakram63@gmail.com>
2026-08-07 21:02:11 +0500

    Init vs deinit

---
+++ Learning/OOP/ObjectOrientedProgramming.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+/*
+
+ // Object Oriented Programming
+ 
+ // During the life the app, we create and destroy objects
+ // Create = Initialize (init) = Allocate (add to memory)
+ // Destroy = Deinitialize (deinit) = Deallocate (remove from memory)
+
+
+
+
+
+*/
