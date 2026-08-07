60af56b56db05e7f18a1f1babf8584c3850356d3
eashaakram <eashaakram63@gmail.com>
2026-08-07 21:20:17 +0500

    Automatic Reference Counting

--- Learning/OOP/ObjectOrientedProgramming.swift
+++ Learning/OOP/ObjectOrientedProgramming.swift
@@ -8,7 +8,18 @@ import Foundation
  // Create = Initialize (init) = Allocate (add to memory)
  // Destroy = Deinitialize (deinit) = Deallocate (remove from memory)
 
+ // Automatic Reference Counting (ARC)
+ // A live count of the number of objects in memory
+ // Create 1 object, count goes up by 1
+ // Create 2 objects, count goes up by 2
+ // Destroy 1 object, count goes down by 1
 
+ // The more objects in memory, the slower the app performs
+ // We want to keep the ARC count as low as possible
+ // We want to create objects only when we need them
+ // And destroy them as soon as we no longer need them
+
+ // For example, if an app has 2 screens and user is moving from screen 1 to screen 2. We only want to allocate screen 2 WHEN we need it (ie. when the user clicks a button to segue to screen 2). When we get to screen 2, we may want to deallocate screen 1.
 
 
 
