151a9e4ddc332440ebfe1e150022bed68dda4ef8
eashaakram <eashaakram63@gmail.com>
2026-08-09 23:22:34 +0500

    explained to a 5-year old

--- Learning/OOP/ObjectOrientedProgramming.swift
+++ Learning/OOP/ObjectOrientedProgramming.swift
@@ -55,6 +55,34 @@ import Foundation
  // Objects in the Heap are "Reference" types.
  // When you edit a Refernce type, you edit the object that you are referencing. This "reference" is called "pointer" because it "points" to an object in the Heap (in memory).
 
-
-
 */
+
+
+struct MyFirstObject {
+    let title: String = "Hello, world!"
+}
+
+class MySecondObject {
+    let title: String = "Hello, world!"
+}
+
+// Class vs Struct explained to a 5-year old
+
+// Imagine a school and in the school there are classrooms.
+// Within each class, there are quizzes.
+// During the day, the teacher will hand out many different quizzes to different classes. The students will answer the quizzes and return them back to the teacher.
+
+// "school" = App
+// "classroom" = Class (classroom is not moving it is in place we do actions in class one of the action is quiz(Struct) everytime we handout quizes we're going to make 50 copies of quizzes that is value type)
+// "quiz" = Struct
+
+// In this example, we have a classroom and there are many actions that occur inside the classroom.
+// In code, we create a class and can perform actions within class.
+
+// In this example, there are many different type of quizzes. The teacher hands out the quizzes and the students take the quizzes and return them to the teacher.
+// In code, we create many structs and pass then around out app with ease.
+
+
+// Note:
+// This metaphor is NOT perfect :)
+ // Technical a "quiz" can be a class, etc.
