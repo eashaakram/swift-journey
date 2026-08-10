104c40bb48669f105b7abd27ebe6041b418896e3
eashaakram <eashaakram63@gmail.com>
2026-08-11 01:19:44 +0500

    Sturcts

---
+++ Learning/OOP/Structs.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+
+// Structs are fast!
+// Structs are stored in the Stack (memory)
+// Objects in the Stack are Value types
+// Value types are copied & mutated
+
+struct Quiz {
+    let title: String 
+    let dateCreated: Date
+}
+
+let myObject: String = "Hello, world!"
+
+let myQuiz: Quiz = Quiz(title: "Quiz 1", dateCreated: .now)
+// myQuiz is of type Quiz and Quiz() is function and inside() it have parameters
+print(myQuiz.title)
