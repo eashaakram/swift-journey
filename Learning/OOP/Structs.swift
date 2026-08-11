4b7fb0d0e0e620e2647f0ccf2ca5a25139aa4694
eashaakram <eashaakram63@gmail.com>
2026-08-11 19:09:06 +0500

    Customizing init

--- Learning/OOP/Structs.swift
+++ Learning/OOP/Structs.swift
@@ -9,16 +9,39 @@ import Foundation
 struct Quiz {
     let title: String 
     let dateCreated: Date
+    let isPremium: Bool? 
 
-    // Structs have an implicit init
-    init(title: String, dateCreated: Date) {
+    // // Structs have an implicit init
+    // init(title: String, dateCreated: Date) {
+    //     self.title = title
+    //     self.dateCreated = dateCreated
+    // }
+
+    // // Customizing init 
+    // init(title: String, dateCreated: Date = .now) {
+    //     self.title = title
+    //     self.dateCreated = dateCreated
+    // }
+
+     init(title: String, dateCreated: Date?, isPremium: Bool? ) {
         self.title = title
-        self.dateCreated = dateCreated
+        self.dateCreated = dateCreated ?? .now
+        self.isPremium = isPremium
     }
 }
 
 let myObject: String = "Hello, world!"
 
-let myQuiz: Quiz = Quiz(title: "Quiz 1", dateCreated: .now)
-// myQuiz is of type Quiz and Quiz() is function and inside() it have parameters
+//let myQuiz: Quiz = Quiz(title: "Quiz 1", dateCreated: .now)
+// // myQuiz is of type Quiz and Quiz() is function and inside() it have parameters
+
+// // if we customize init 
+// let myQuiz: Quiz = Quiz(title: "Quiz 1!")
+// // here Quiz(...) is Quiz instance/object
+
+//let myQuiz: Quiz = Quiz(title: "Quiz 1", isPremium: nil)
+let myQuiz: Quiz = Quiz(title: "Quiz 1", dateCreated: nil , isPremium: false)
+
 print(myQuiz.title)
+print(myQuiz.dateCreated)
+print(myQuiz.isPremium)
