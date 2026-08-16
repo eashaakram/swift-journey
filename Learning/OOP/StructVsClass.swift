0d28bb872c52a4cab74475d7ed07187b0fd30a91
eashaakram <eashaakram63@gmail.com>
2026-08-17 01:44:03 +0500

    Struct vs Class

--- Learning/OOP/StructVsClass.swift
+++ Learning/OOP/StructVsClass.swift
@@ -88,8 +88,36 @@ func forClass() {
     let fName = MyClass(name: "iOS Swift")
     let sName = fName
     sName.name = "iOS Objective-C"
+    // let reference ko constant bana raha hai.
+    // Matlab:
+    // sName ko kisi doosre object par point nahi karwa sakti.
+    // For example, ye nahi kar sakti:
+    // sName = MyClass(name: "Hello")   
+    // Because sName is let.
+    // Lekin:
+    // sName.name = "iOS Objective-C"
+    // because tum sName ko replace nahi kar rahi.
+    // Tum us object ke andar ki property change kar rahi ho.
     print(fName.name)
     print(sName.name)
 }
 
 forClass()
+
+/*
+NOTE
+
+fName ───────┐
+             ↓
+        ┌─────────────┐
+        │ MyClass     │
+        │ name=Swift  │
+        └─────────────┘
+             ↑
+sName ───────┘
+
+Ek object. Do references.
+means:
+fName jis object ko refer kar raha hai, sName ko bhi usi object ka reference de do.
+
+*/
