5c1bbd10df8e1c66ecd0660e4d87421bb59f0bb7
eashaakram <eashaakram63@gmail.com>
2026-08-29 16:45:39 +0500

    Filter

--- Learning/OOP/FilterSortMap.swift
+++ Learning/OOP/FilterSortMap.swift
@@ -9,8 +9,34 @@ struct DatabaseUser {
 
 var allUsers: [DatabaseUser] = [
     DatabaseUser(name: "Nick", isPremium: true, order: 4),
-    DatabaseUser(name: String, isPremium: Bool, order: 1),
-    DatabaseUser(name: String, isPremium: Bool, order: 3),
-    DatabaseUser(name: String, isPremium: Bool, order: Int),
-    DatabaseUser(name: String, isPremium: Bool, order: Int),
+    DatabaseUser(name: "Emily", isPremium: false, order: 1),
+    DatabaseUser(name: "Samatha", isPremium: true, order: 3),
+    DatabaseUser(name: "Joe", isPremium: true, order: 1000),
+    DatabaseUser(name: "Chris", isPremium: false, order: 2),
 ]
+
+// Filter
+
+// var allPremiumUsers: [DatabaseUser] = allUsers.filter { user in  
+//     if user.isPremium {
+//         return true
+//     }
+//     return false
+// }
+
+var allPremiumUsers: [DatabaseUser] = allUsers.filter { user in
+    user.isPremium
+}
+
+// more short to write (same as above)
+var allPremiumUsers2: [DatabaseUser] = allUsers.filter({ $0.isPremium })
+
+// var allPremiumUsers: [DatabaseUser] = []
+
+// for user in allUsers {
+//     if user.isPremium {
+//         allPremiumUsers.append(user)
+//     }
+// }
+print((allPremiumUsers))
+print("2: \(allPremiumUsers2)")
