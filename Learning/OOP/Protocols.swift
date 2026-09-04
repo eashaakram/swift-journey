d0370c43b682d6f04457afe5e24fc66cd839e7f4
Easha Akram <eashaakram63@gmail.com>
2026-09-04 19:38:13 +0500

    protocols

--- Learning/OOP/Protocols.swift
+++ Learning/OOP/Protocols.swift
@@ -1,1 +1,12 @@
 import Foundation
+
+// Here EmployeeModel is conforming to the protocol EmployeeHasAName, which means it must have a property called name of type String. The protocol acts as a blueprint that defines what properties or methods a conforming type must implement. 
+struct EmployeeModel : EmployeeHasAName {
+    let title: String 
+    let name: String
+}
+
+// Its a rule and anything that conforms to this protocol must have whatever we put in here
+protocol EmployeeHasAName {
+    var name: String 
+}
